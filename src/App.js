c00e4d198f3109c6148ec1b64ac59175b21802b3
fuston05 <fuston05@yahoo.com>
2019-12-18 10:14:47 -0500

    finished 2 stretch goals: added button for quarters, and separated "top row out into its own component"

--- src/App.js
+++ src/App.js
@@ -2,33 +2,27 @@
 import React, {useState} from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
+import TopRow from "./TopRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [homeScore, setHomeScore]= useState(0);
   const [awayScore, setAwayScore]= useState(0);
+  const [quarter, setQuarter]= useState(0);
   return (
     <div className="container">
       <section className="scoreboard">
-        <div className="topRow">
-          <div className="home">
-            <h2 className="home__name">Lions</h2>
-
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-
-            <div className="home__score">{homeScore}</div>
-          </div>
-          <div className="timer">00:03</div>
-          <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">{awayScore}</div>
-          </div>
-        </div>
-        <BottomRow />
+        
+        {/* stretch: separate into smaller components */}
+        <TopRow homeScore= {homeScore} awayScore= {awayScore} />
+        <BottomRow quarter= {quarter} />
       </section>
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
+
+          {/* Stretch: button to increment quarter */}
+          <button className="quarter" onClick= {() => {setQuarter(quarter +1)}}>Set Quarter</button>
           <button className="homeButtons__touchdown" onClick= {() => {setHomeScore(homeScore + 7)}}>Home Touchdown</button>
           <button className="homeButtons__fieldGoal" onClick= {() => {setHomeScore(homeScore + 3)}}>Home Field Goal</button>
         </div>
